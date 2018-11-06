da8b00f76429fd4a31a6e5df04a9298f6501077e
Anton19801310 <rgn78@yandex.ru>
2018-11-06 21:14:26 +0300

    4.1. Глупый бот. [#31323]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -18,34 +18,16 @@ public class Point{
 	}
 
 	public double distanceTo(Point that){
-
-		//Point a = this;
-		//Point b = that;
-
-		//int x1 = a.x;
-		//int y1 = a.y;
-		//int x2 = b.x;
-		//int y2 = b.y;
-		//double result = Math.sqrt(
-		//		Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
-		//);
-		//return result;
-
 		return Math.sqrt(
 				Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
 		);
 
-		//System.out.println("x1 = " + a.x);
-		//System.out.println("y1 = " + a.y);
-		//System.out.println("x2 = " + b.x);
-		//System.out.println("y2 = " + b.y);
-		//return -1;
 	}
 
 	public static void main(String[] args){
 		Point a = new Point(0, 1);
 		Point b = new Point(2, 5);
-		//a.distanceTo(b);
+
 		System.out.println("x1 = " + a.x);
 		System.out.println("y1 = " + a.y);
 		System.out.println("x2 = " + b.x);
