6db1b373423c1337eacc06a23e5be83374262162
Anton19801310 <rgn78@yandex.ru>
2018-11-13 19:54:46 +0300

    4.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,20 @@
+package ru.job4j.max;
+
+/**
+* Package for max task.
+*
+* @author Anton Lyalin 
+* @version $Id$
+* @since 0.1
+*/
+
+public class Max {
+	/**
+     * Method max.
+     * @param first, second.
+     * @return max.
+     */
+    public int max(int first, int second) {
+        return (first > second ? first : second);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/max/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for maxtest task.
+*
+* @author Anton Lyalin 
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.max;
