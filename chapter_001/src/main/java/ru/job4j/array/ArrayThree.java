1c0ef5e095e4d9d6a45e0254c21f9aef476f68e7
Anton19801310 <rgn78@yandex.ru>
2019-03-12 21:20:52 +0300

    Тестовое задание. Слияние двух массивов.

--- chapter_001/src/main/java/ru/job4j/array/ArrayThree.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayThree.java
@@ -11,16 +11,36 @@ import java.util.Arrays;
 */
 
 public class ArrayThree {
+    public int[] mergeArrays(int[] a, int[] b) {
 
-        public static int[] mergeArrays(int[] a, int[] b) {
+        int[] result = new int[a.length + b.length];
+                int i = 0, j = 0, k = 0;
 
-        int[] result = new int [a.length + b.length];
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                result[k] = a[i];
+                i++;
+            } else {
+                result[k] = b[j];
+                j++;
+            }
+            k++;
+        }
 
-        System.arraycopy(a, 0, result, 0, a.length);
-        System.arraycopy(b, 0, result, a.length, b.length);
+        while (i < a.length){
+            result[k] = a[i];
+            i++;
+            k++;
+        }
+
+        while (j < b.length){
+            result[k] = b[j];
+            j++;
+            k++;
+        }
 
         return result;
-        }
+    }
 
 
 }
