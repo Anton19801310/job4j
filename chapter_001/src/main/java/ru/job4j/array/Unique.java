b7a811705baf33c25cb215392a230744c84ff126
Anton19801310 <rgn78@yandex.ru>
2019-03-25 21:44:21 +0300

    Проверить. Что два массива состоят из одинаковых символов.

--- chapter_001/src/main/java/ru/job4j/array/Unique.java
+++ chapter_001/src/main/java/ru/job4j/array/Unique.java
@@ -9,20 +9,25 @@ package ru.job4j.array;
 */
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Проверить. Что два массива состоят из одинаковых символов.
  */
 public class Unique {
-    public boolean unique(int[] value1, int[] value2) {
-        Arrays.sort(value1);
-        Arrays.sort(value2);
-        boolean rs = false;
-        for (int index = 0; index < value1.length - 1 && index < value2.length -1; index++) {
-            if (value1[index] == value2[index]) {
-                rs = true;
-                break;
-            }
+    public boolean unique(int[] array1, int[] array2) {
+        boolean rs = true;
+        Map<int[], Integer> map1 = new HashMap();
+        for (int i = 0; i < array1.length; i++) {
+            map1.put(array1, i);
+        }
+        Map<int[], Integer> map2 = new HashMap();
+        for (int i = 0; i < array2.length; i++) {
+            map2.put(array1, i);
+        }
+        if (!map1.equals(map2)) {
+            rs = false;
         }
         return rs;
     }
